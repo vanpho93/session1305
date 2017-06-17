3c67c7b01acfd15815e50f3e6561fa3c52a6bb26
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-17 16:10:29 +0700

    ket thuc

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const session = require('express-session');
 const parser = require('body-parser').urlencoded({ extended: false });
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const User = require('./User');
 
+const SECKET_KEY = 'kwqeb812hawfh8qfqw';
 const app = express();
 app.listen(process.env.PORT || 3000, () => console.log('Server started'));
 app.use(express.static('public'));
@@ -20,7 +22,11 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.render('home', { name: req.session.name });
+    if (!req.cookies.TOKEN) return res.render('home', { name: undefined });
+    jwt.verify(req.cookies.TOKEN, SECKET_KEY, (err, obj) => {
+        if (err) return res.render('home', { name: undefined });
+        res.render('home', obj);
+    });
 });
 
 app.get('/signUp', (req, res) => res.render('signUp'));
@@ -39,21 +45,27 @@ app.post('/signIn', parser, (req, res) => {
     const user = new User(email, password);
     user.signIn()
     .then((name) => {
-        req.session.name = name; // eslint-disable-line
-        res.send('DANG_NHAP_THANH_CONG');
+        jwt.sign({ name }, SECKET_KEY, (err, token) => {
+            res.cookie('TOKEN', token);
+            res.send('DANG_NHAP_THANH_CONG');
+        });
     })
     .catch(() => res.send('DANG_NHAP_THAT_BAI'));
 });
 
 // app.get('/muave', (req, res) => {
-//     req.session.daMuaVe = true; // eslint-disable-line
-//     res.send('Da mua ve');
+//     jwt.sign({ daMuaVe: true }, SECKET_KEY, (err, token) => {
+//         res.cookie('TOKEN', token);
+//         res.send('Da mua ve');
+//     });
 // });
 
 // app.get('/vaorap', (req, res) => {
-//     req.session.count = req.session.count ? ++req.session.count : 1;// eslint-disable-line
-//     if (!req.session.daMuaVe) return res.send('Hay di mua ve');
-//     res.send('Welcome');
+//     if (!req.cookies.TOKEN) return res.send('BAN PHAI MUA VE');
+//     jwt.verify(req.cookies.TOKEN, SECKET_KEY, (err, obj) => {
+//         if (err) return res.send('BAN PHAI MUA VE');
+//         res.send('WELCOME');
+//     });
 // });
 
 app.use((err, req, res, next) => res.send('LOI')); // eslint-disable-line
